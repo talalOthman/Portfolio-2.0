54b3c94cf403c158d06d8032a5848b260010b5b5
Talal <TalalOthman2000@gmail.com>
2021-03-05 00:24:59 +0800

    added 'visit here' option to all projects

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -265,8 +265,8 @@ https://templatemo.com/tm-528-elegance
                                                 <img src="images/item-1.jpg" alt="">
                                             </div>
                                             <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap CSS templates.</p>
+                                                <h4>Shared Learning Web Application</h4>
+                                                <p>A calendar web app created using <strong>PHP framework Laravel.</strong> This project is using several social media platform APIs for Authenticaion<br/><a href="http://slwa.herokuapp.com" class="project-link">Visit here</a></p>
                                             </div>
                                         </div>
                                     </div>
@@ -276,8 +276,8 @@ https://templatemo.com/tm-528-elegance
                                                 <img src="images/item-2.jpg" alt="">
                                             </div>
                                             <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap themes.</p>
+                                                <h4>Covid-19 Tracker</h4>
+                                                <p>A web application created by using <strong>Javascript framework REACT</strong>. This project tracks the covid 19 cases by fetching data from an API.<br/><a href="http://slwa.herokuapp.com" class="project-link">Visit here</a></p>
                                             </div>
                                         </div>
                                     </div>
@@ -287,8 +287,8 @@ https://templatemo.com/tm-528-elegance
                                                 <img src="images/item-3.jpg" alt="">
                                             </div>
                                             <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap layouts.</p>
+                                                <h4>Leave Application Management System</h4>
+                                                <p>A website created using <strong>vanilla PHP</strong>. This system contains an Authenticaion for several users with different views.<br/><a href="http://slwa.herokuapp.com" class="project-link">Visit here</a></p>
                                             </div>
                                         </div>
                                     </div>
@@ -298,8 +298,8 @@ https://templatemo.com/tm-528-elegance
                                                 <img src="images/item-1.jpg" alt="">
                                             </div>
                                             <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap templates.</p>
+                                                <h4>Sports Venue Booking System</h4>
+                                                <p>A web application created using <strong>vanilla Java</strong>. This system contains an Authenticaion for several different user types.<br/><a href="http://slwa.herokuapp.com" class="project-link">Visit here</a></p>
                                             </div>
                                         </div>
                                     </div>
@@ -309,55 +309,15 @@ https://templatemo.com/tm-528-elegance
                                                 <img src="images/item-2.jpg" alt="">
                                             </div>
                                             <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download Bootstrap CSS templates.</p>
+                                                <h4>Old Portfolio</h4>
+                                                <p>A website created using <strong>CSS, HTML, and Javascript</strong>. No frameworks or libraries were used during the development of this project.<br/><a href="https://talalothman.github.io/Portfolio-Website/" class="project-link">Visit here</a></p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="item animate" data-animate="fadeInUp">
-                                        <div class="portfolio-item">
-                                            <div class="thumb">
-                                                <img src="images/item-3.jpg" alt="">
-                                            </div>
-                                            <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap templates.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item animate" data-animate="fadeInUp">
-                                        <div class="portfolio-item">
-                                            <div class="thumb">
-                                                <img src="images/item-1.jpg" alt="">
-                                            </div>
-                                            <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap templates.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item animate" data-animate="fadeInUp">
-                                        <div class="portfolio-item">
-                                            <div class="thumb">
-                                                <img src="images/item-2.jpg" alt="">
-                                            </div>
-                                            <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap templates.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="item animate" data-animate="fadeInUp">
-                                        <div class="portfolio-item">
-                                            <div class="thumb">
-                                                <img src="images/item-3.jpg" alt="">
-                                            </div>
-                                            <div class="thumb-inner animate" data-animate="fadeInUp">
-                                                <h4>templatemo is the best</h4>
-                                                <p>Please tell your friends about it. Templatemo is the best website to download free Bootstrap templates.</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                                   
+                                   
+                                  
+                            
                                 </div>
                             </div>
                         </div>
